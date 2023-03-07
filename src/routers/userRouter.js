40d0a4a3ee62ec908c62e36634edeceb177e10ac
Nayeob <108695511+Raccoonmen@users.noreply.github.com>
2023-03-07 17:31:46 +0900

    Test-save
    Test-save

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,21 +1,28 @@
 import express from "express";
 import {
-  logout,
-  getEdit,
-  postEdit,
-  see,
-  startGithubLogin,
-  finishGithubLogin,
+    logout,
+    getEdit,
+    postEdit,
+    see,
+    startGithubLogin,
+    finishGithubLogin,
+    getChangePassword,
+    postChangePassword,
 } from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 /**users가 루트URL인  Router */
 const userRouter = express.Router();
 
-userRouter.get("/logout",protectorMiddleware, logout);
+userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
-userRouter.get("/github/start",publicOnlyMiddleware, startGithubLogin);
-userRouter.get("/github/finish", publicOnlyMiddleware,finishGithubLogin);
+userRouter
+    .route("/change-password")
+    .all(protectorMiddleware)
+    .get(getChangePassword)
+    .post(postChangePassword);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id(\\d+)", see);
 
 export default userRouter;
